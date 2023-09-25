bdd8588d8db3c9d8199d1c545bec99ef3c7e2cdd
Parpecito <davidmg26dabio@gmail.com>
2023-09-25 15:49:51 +0200

    Cambio

--- Ejercicio3/ejercicio3.ts
+++ Ejercicio3/ejercicio3.ts
@@ -1,3 +1,4 @@
+//Hacer una función que tenga un parámetro de entrada que sea un array de strings, el cual se deberá ordenar alfabéticamente y devolver el primer valor que haya separando sus letras con espacios.
 const palabras:string[]=["Comida","Cena","Merienda","Piscina","Ordenador","Abeja","Elefante"];
 const orden_devol=(palabras:string[])=>{
     if(palabras.length===0){
@@ -5,7 +6,7 @@ const orden_devol=(palabras:string[])=>{
     }
     const ordenar_palabras=palabras.sort();
     const primer_elemento=ordenar_palabras[0];
-    const separar_palabras=primer_elemento.split('');
+    const separar_palabras=primer_elemento.split('').join('');
     return separar_palabras;
 }
 
